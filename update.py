3708ecd944fbf4167eafcc270b92985bf8affa94
lyneca <lukemtuthill@gmail.com>
2017-07-02 13:51:55 +1000

    lined up progress text on update.py

--- update.py
+++ update.py
@@ -1,20 +1,26 @@
 from bs4 import BeautifulSoup
 from mod import EnemyModDrop
 import requests
+import sys
 import os
+print('Downloading page...    ', end='')
+sys.stdout.flush()
 r = requests.get('http://n8k6e2y6.ssl.hwcdn.net/repos/hnfvc0o3jnfvc873njb03enrf56.html')
+print('done.')
+print('Parsing...             ', end='')
+sys.stdout.flush()
 soup = BeautifulSoup(r.content.decode(), "html5lib")
+print('done.')
+print('Extracting mod info... ', end='')
+sys.stdout.flush()
 mod_locations = soup.find_all(id='modLocations')[0].next_sibling
 mod_locations = next(mod_locations.children).contents
 i = -1
 mods = {}
-print([x for x in mod_locations[:5]])
 for element in mod_locations:
-    print(element.name, len(element.contents))
     if 'class' in element and element['class'] == 'blank-row':
         continue 
     if element.contents[0].name == 'th' and len(element.contents) == 1:
-        print('boop')
         current_element = element.string
         mods[current_element] = []
     elif len(element.contents) == 3 and not element.contents[0].name == 'th':
@@ -24,6 +30,12 @@ for element in mod_locations:
             float(element.contents[2].string.split()[-1][1:-2]),
             ' '.join(element.contents[2].string.split()[:-1])
         ))
-if not os.path.exists('mods.db'): open('mods.db', 'x').close()
+print('done.')
+print('Writing to file...     ', end='')
+sys.stdout.flush()
 
+if not os.path.exists('mods.db'): open('mods.db', 'x').close()
 open('mods.db', 'w').write(repr(mods))
+
+print('done.')
+print('finished.')
